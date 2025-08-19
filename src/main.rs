22afe98749bff4523d1ffafcbff1eeeebcc39ac4
Cameron <cameronpool2019@gmail.com>
2025-08-19 10:45:47 -0500

    basic handling of cli args for "server" or "client"  and basic error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
+use std::env;
+use std::io;
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(stream: TcpStream) {
-    println!("connected..")
+fn connect_client() -> io::Result<()> {
+    println!("connecting client");
+    Ok(())
 }
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:80")?;
+fn start_server() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:5000")?;
 
-    // accept connections and process them serially
     for stream in listener.incoming() {
         handle_client(stream?);
     }
     Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) {
+    println!("client has connected");
+    println!("{stream:?}");
+}
+
+fn main() -> std::io::Result<()> {
+    let arg = env::args().nth(1);
+
+    match arg.as_deref() {
+        Some("server") => start_server(),
+        Some("client") => connect_client(),
+        _ => Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Please enter server or client",
+        )),
+    }
 }
